05af04725e00fab1ef9bc188907221c4c289737e
Dante Guerrero <dante.guerrerobarreto@gmail.com>
2021-06-09 22:53:49 -0500

    Agregué un poco de orden a apoyo.formato

--- apoyo/formato.py
+++ apoyo/formato.py
@@ -1,11 +1,17 @@
 ########################################################################
 
+# GENERALES
+
 fondo = '#f0f0f0'
+
+# BOTONES
 
 boton_cuando_pasa_cursor = '#b5c8e5'
 boton_sin_que_pase_cursor = '#144ca4'
 letras_del_boton = '#ffffff'
 letras_del_boton_cuando_pasa_cursor = '#000000'
+
+# TABLAS (Escenario)
 
 fondo_encabezados_de_tabla = '#747475'
 tipo_de_letra_tabla = 'helvetica 8'
